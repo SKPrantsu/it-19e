71aad40377c7fd7015d1904c942c90e7400cd3b8
SKPrantsu <Sten-Kristjan.Prantsu@tptlive.ee>
2022-04-05 09:43:40 +0300

    Update tund4-labyrinth.py

--- tund4-labyrinth.py
+++ tund4-labyrinth.py
@@ -5,8 +5,22 @@ map = [
 [0, 0, 1, 0, 0], 
 [0, 0, 0, 0, 24]]
 
+vaba_tee = 0
+sein = 1
+
 start_x = 0
 start_y = 0
+
+def saab_liikuda_paremale(map, praegused_koordinaadid):
+    parempool = praegused_koordinaadid[0] + 1
+    praegune_rida = praegused_koordinaadid[1]
+    if (map[praegune_rida][parempool] == vaba_tee):
+        return True
+    else:
+        return False
+
+print(saab_liikuda_paremale(map, [start_x, start_y]))
+print(saab_liikuda_paremale(map, [1, 1]))
 
 def print_map():
     for rida in map:
@@ -16,4 +30,8 @@ def print_map():
             taisrida += str(element) + " "
         print(taisrida)
 
-print_map()
+def print_map2():
+    for x in map:
+        print(x)
+
+print_map2()
